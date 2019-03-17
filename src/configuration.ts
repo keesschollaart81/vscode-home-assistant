0ea874726ad8fa8f9e105e8e2c5d438696979ee1
Kees Schollaart <keesschollaart81@hotmail.com>
2019-03-17 15:21:14 +0100

    Moving Config to own class

--- src/configuration.ts
+++ src/configuration.ts
@@ -9,5 +9,38 @@ export const config = {
     },
     get haToken() {
         return config.all.get<string>("ha-token");
+    },
+    async trySetConfig(): Promise<boolean> {
+        var optionClicked = await vscode.window.showInformationMessage(
+            "Update your settings to integrate with Home Assistant",
+            "Now",
+            "Later");
+
+        if (optionClicked === "Later") {
+            return false;
+        }
+
+        var url = await vscode.window.showInputBox(<vscode.InputBoxOptions>{
+            prompt: "Enter your Home Assistant (base) URL",
+            placeHolder: "https://your.homeassistant.com:8123"
+        });
+        if (!url) {
+            return false;
+        }
+
+        var token = await vscode.window.showInputBox(<vscode.InputBoxOptions>{
+            prompt: "Enter a Home Assistant 'Long-Lived Access Token', create one on your user/profile page in HA.",
+            password: true
+        });
+        if (!token) {
+            return false;
+        }
+
+        let config = vscode.workspace.getConfiguration("vscode-home-assistant");
+        await config.update("ha-url", url);
+        await config.update("ha-token", token); 
+        
+        return true;
     }
 };
+
--- src/homeassistant.ts
+++ src/homeassistant.ts
@@ -13,7 +13,7 @@ export class HomeAssistant {
         return new Promise(async (resolve, reject) => {
 
             if (!config.haUrl || !config.haToken) {
-                let didUpdateConfig = await this.updateConfig();
+                let didUpdateConfig = await config.trySetConfig();
                 if (!didUpdateConfig){
                     return reject();
                 } 
@@ -87,40 +87,7 @@ export class HomeAssistant {
             completions.push(completionItem);
         }
         return completions;
-    }
-
-    private async updateConfig(): Promise<boolean> {
-        var optionClicked = await vscode.window.showInformationMessage(
-            "Update your settings to integrate with Home Assistant",
-            "Now",
-            "Later");
-
-        if (optionClicked === "Later") {
-            return false;
-        }
-
-        var url = await vscode.window.showInputBox(<vscode.InputBoxOptions>{
-            prompt: "Enter your Home Assistant (base) URL",
-            placeHolder: "https://your.homeassistant.com:8123"
-        });
-        if (!url) {
-            return false;
-        }
-
-        var token = await vscode.window.showInputBox(<vscode.InputBoxOptions>{
-            prompt: "Enter a Home Assistant 'Long-Lived Access Token', create one on your user/profile page in HA.",
-            password: true
-        });
-        if (!token) {
-            return false;
-        }
-
-        let config = vscode.workspace.getConfiguration("vscode-home-assistant");
-        await config.update("ha-url", url);
-        await config.update("ha-token", token); 
-        
-        return true;
-    }
+    } 
 }
 
 export class HomeAssistantCompletionItem extends CompletionItem {
