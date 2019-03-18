0b250c8cd4f3d1f9b31d398f180332aea2225392
Kees Schollaart <keesschollaart81@hotmail.com>
2019-03-18 21:52:07 +0100

    Service Completions

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,26 @@ import * as vscode from 'vscode';
 import { EntityIdCompletionProvider } from './entity-id-completion-provider';
 import { HomeAssistant } from './homeassistant';
 import { config } from './configuration';
+import { ServiceCompletionProvider } from './service-completion-provider';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	let ha = new HomeAssistant();
 
-	let completionProvider = vscode.languages.registerCompletionItemProvider(
+	let entityCompletionProvider = vscode.languages.registerCompletionItemProvider(
 		'yaml',
 		new EntityIdCompletionProvider(ha),
 		":",
 		"-"
 	);
+	let serviceCompletionProvider = vscode.languages.registerCompletionItemProvider(
+		'yaml',
+		new ServiceCompletionProvider(ha),
+		":"
+	);
 
-	context.subscriptions.push(completionProvider);
+	context.subscriptions.push(entityCompletionProvider);
+	context.subscriptions.push(serviceCompletionProvider);
 
 	config.hasConfigOrAsk();
 }
--- src/homeassistant.ts
+++ src/homeassistant.ts
@@ -8,52 +8,64 @@ const WebSocket = require("ws");
 export class HomeAssistant {
 
     connection: ha.Connection | undefined;
-    hassEntities: ha.HassEntities | undefined;
+    hassEntities!: Promise<ha.HassEntities>;
+    hassServices!: Promise<ha.HassServices>;
 
     private async ensureConnection(): Promise<void> {
-        return new Promise(async (resolve, reject) => {
 
-            let hasConfig = await config.hasConfigOrAsk();
-            if (!hasConfig) {
+        let hasConfig = await config.hasConfigOrAsk();
+        if (!hasConfig) {
+            return;
+        }
+
+        if (this.connection) {
+            return;
+        }
+
+        let auth = new ha.Auth(<ha.AuthData>{
+            access_token: config.haToken,
+            expires: +new Date(new Date().getTime() + 1e11),
+            hassUrl: config.haUrl,
+            clientId: "",
+            expires_in: +new Date(new Date().getTime() + 1e11),
+            refresh_token: ""
+        });
+
+        try {
+            console.log("Connecting to Home Assistant...");
+            this.connection = await ha.createConnection({ auth, WebSocket });
+        }
+        catch (error) {
+            this.connection = undefined;
+            vscode.window.showErrorMessage(`Error connecting to your Home Assistant Server at ${config.haUrl}, check your network or update your VS Code Settings. Error: ${error}`);
+            console.error(error);
+            throw error;
+        }
+
+        this.connection.addEventListener("ready", () => {
+            console.log("Connected to Home Assistant");
+        });
+
+        this.connection.addEventListener("disconnected", () => {
+            console.log("Lost connection with Home Assistant");
+        });
+
+        this.hassEntities = new Promise<ha.HassEntities>(async (resolve, reject) => {
+            if (!this.connection) {
                 return reject();
             }
-
-            if (this.connection) {
-                return resolve();
-            }
-
-            let auth = new ha.Auth(<ha.AuthData>{
-                access_token: config.haToken,
-                expires: +new Date(new Date().getTime() + 1e11),
-                hassUrl: config.haUrl,
-                clientId: "",
-                expires_in: +new Date(new Date().getTime() + 1e11),
-                refresh_token: ""
-            });
-
-            try {
-                console.log("Connecting to Home Assistant...");
-                this.connection = await ha.createConnection({ auth, WebSocket });
-            }
-            catch (error) {
-                this.connection = undefined;
-                vscode.window.showErrorMessage(`Error connecting to your Home Assistant Server at ${config.haUrl}, check your network or update your VS Code Settings. Error: ${error}`);
-                console.error(error);
-                return reject(error);
-            }
-
-            this.connection.addEventListener("ready", () => {
-                console.log("Connected to Home Assistant");
-            });
-
-            this.connection.addEventListener("disconnected", () => {
-                console.log("Lost connection with Home Assistant");
-            });
-
             ha.subscribeEntities(this.connection, entities => {
-                console.log(`Got ${Object.keys(entities).length} from Home Assistant`);
-                this.hassEntities = entities;
-                return resolve();
+                console.log(`Got ${Object.keys(entities).length} entities from Home Assistant`);
+                return resolve(entities);
+            });
+        });
+        this.hassServices = new Promise<ha.HassServices>(async (resolve, reject) => {
+            if (!this.connection) {
+                return reject();
+            }
+            ha.subscribeServices(this.connection, services => {
+                console.log(`Got ${Object.keys(services).length} services from Home Assistant`);
+                return resolve(services);
             });
         });
     }
@@ -61,13 +73,13 @@ export class HomeAssistant {
     public async getEntityCompletions(): Promise<HomeAssistantCompletionItem[]> {
         await this.ensureConnection();
 
-        if (!this.hassEntities) {
+        if (!await this.hassEntities) {
             return [];
         }
 
         let completions: HomeAssistantCompletionItem[] = [];
 
-        for (const [key, value] of Object.entries(this.hassEntities)) {
+        for (const [key, value] of Object.entries(await this.hassEntities)) {
             let completionItem = new HomeAssistantCompletionItem(` ${value.entity_id}`, vscode.CompletionItemKind.EnumMember);
 
             completionItem.documentation = new vscode.MarkdownString(`**${value.entity_id}** \r\n \r\n`);
@@ -82,6 +94,37 @@ export class HomeAssistant {
             }
             completions.push(completionItem);
         }
+        return completions;
+    }
+
+    public async getServiceCompletions(): Promise<HomeAssistantCompletionItem[]> {
+        await this.ensureConnection();
+
+        if (!this.hassServices) {
+            return [];
+        }
+
+        let completions: HomeAssistantCompletionItem[] = [];
+
+        for (const [domainKey, domainValue] of Object.entries(await this.hassServices)) {
+            for (const [serviceKey, serviceValue] of Object.entries(domainValue)) {
+                let completionItem = new HomeAssistantCompletionItem(` ${domainKey}.${serviceKey}`, vscode.CompletionItemKind.EnumMember);
+
+                var fields = Object.entries(serviceValue.fields);
+
+                if (fields.length > 0) {
+                    completionItem.documentation = new vscode.MarkdownString(`**${domainKey}.${serviceKey}:** \r\n \r\n`);
+                    completionItem.documentation.appendMarkdown(`| Field | Description | Example | \r\n`);
+                    completionItem.documentation.appendMarkdown(`| :---- | :---- | :---- | \r\n`);
+
+                    for (const [fieldKey, fieldValue] of fields) {
+                        completionItem.documentation.appendMarkdown(`| ${fieldKey} | ${fieldValue.description} |  ${fieldValue.example} | \r\n`);
+                    }
+                }
+                completions.push(completionItem);
+            }
+        }
+
         return completions;
     }
 }
