1db58b60227002496963a1b0bd40003018bf52e1
Kees Schollaart <keesschollaart81@hotmail.com>
2019-04-03 21:36:57 +0200

    initial split between language server and client (does not work yet)

---
+++ src/server/src/server.ts
@@ -0,0 +1,26 @@
+import {
+	createConnection, TextDocuments, ProposedFeatures, TextDocumentSyncKind
+} from 'vscode-languageserver';
+
+let connection = createConnection(ProposedFeatures.all);
+let documents = new TextDocuments();
+let workspaceFolder: string | null;
+
+documents.onDidOpen((event) => {
+	connection.console.log(`[Server(${process.pid}) ${workspaceFolder}] Document opened: ${event.document.uri}`);
+})
+documents.listen(connection);
+
+connection.onInitialize((params) => {
+	workspaceFolder = params.rootUri;
+	connection.console.log(`[Server(${process.pid}) ${workspaceFolder}] Started and initialize received`);
+	return {
+		capabilities: {
+			textDocumentSync: {
+				openClose: true,
+				change: TextDocumentSyncKind.None
+			}
+		}
+	}
+});
+connection.listen();
