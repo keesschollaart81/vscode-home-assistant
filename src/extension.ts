458052c472d93bd9b2493ce14e0161600c6bd50e
Kees Schollaart <keesschollaart81@hotmail.com>
2019-04-07 15:22:35 +0200

    Working language server

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,28 @@
+import * as path from 'path';
 import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, TransportKind, ServerOptions } from 'vscode-languageclient';
 import { EntityIdCompletionProvider } from './entity-id-completion-provider';
 import { HomeAssistant } from './homeassistant';
 import { Config }  from './configuration';
 import { ServiceCompletionProvider } from './service-completion-provider';
+
+const documentSelector = [
+    { language: 'yaml', scheme: 'file' },
+    { language: 'yaml', scheme: 'untitled' }
+];
 
 export function activate(context: vscode.ExtensionContext) {
 	let config = new Config();
 	let ha = new HomeAssistant(config);
 
 	let entityCompletionProvider = vscode.languages.registerCompletionItemProvider(
-		'yaml',
+		documentSelector,
 		new EntityIdCompletionProvider(ha),
 		":",
 		"-"
 	);
 	let serviceCompletionProvider = vscode.languages.registerCompletionItemProvider(
-		'yaml',
+		documentSelector,
 		new ServiceCompletionProvider(ha),
 		":"
 	);
@@ -29,6 +36,27 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(serviceCompletionProvider);
 
 	config.hasConfigOrAsk();
+
+   // start language client
+   var serverModule = path.join(context.extensionPath, 'out', 'server', 'server.js');
+
+   var debugOptions = { execArgv: ['--nolazy', "--inspect=6003"] };
+
+   var serverOptions: ServerOptions = {
+	   run: { module: serverModule, transport: TransportKind.ipc },
+	   debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+   };
+
+   var clientOptions: LanguageClientOptions = {
+	   documentSelector,
+	   synchronize: {
+		   configurationSection: 'vscode-home-assistant',
+		   fileEvents: vscode.workspace.createFileSystemWatcher('**/*.?(e)y?(a)ml')
+	   }
+   };
+
+   var client = new LanguageClient('home-assistant', 'Home Assistant Language Server', serverOptions, clientOptions);
+   context.subscriptions.push(client.start());
 }
 
 export function deactivate() { }
