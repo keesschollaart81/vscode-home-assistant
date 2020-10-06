e432ecbbf1c02591e7350991c0f3f8b65bee07eb
Franck Nijhof <git@frenck.dev>
2020-10-06 21:51:15 +0200

    Add schema for template vacuum

---
+++ src/language-service/src/schemas/integrations/vacuum.ts
@@ -0,0 +1,23 @@
+/**
+ * Vacuum integration
+ * Source: https://github.com/home-assistant/core/blob/dev/homeassistant/components/vacuum/__init__.py
+ */
+import { IncludeList } from "../types";
+import { PlatformSchema } from "../platform";
+import { VacuumPlatformSchema as TemplatePlatformSchema } from "./template";
+
+export type Domain = "vacuum";
+export type Schema = Item[] | IncludeList;
+export type File = Item | Item[];
+
+/**
+ * @TJS-additionalProperties true
+ */
+interface OtherPlatform extends PlatformSchema {
+  /**
+   * @TJS-pattern ^(?!template)\w+$
+   */
+  platform: string;
+}
+
+type Item = TemplatePlatformSchema | OtherPlatform;
