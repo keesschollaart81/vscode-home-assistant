4ce427cb1c794060dec7880fb9766c427b1a4bbf
Thibaut Etienne <thibaut@etienne.pw>
2020-10-22 10:05:11 +0200

    Add documentation

--- src/language-service/src/schemas/triggers.ts
+++ src/language-service/src/schemas/triggers.ts
@@ -374,6 +374,19 @@ interface ZoneTrigger {
 }
 
 interface TagTrigger {
+
+  /**
+   * This trigger fired when a tag is scanned.
+   */
   platform: "tag";
+
+  /**
+   * Identifier of the tag. Use this to decide what to do.
+   */
   tag_id: string;
+
+  /**
+   * Device registry identifier of the device that scanned the tag. Use this to decide where to do it.
+   */
+  device_id?: string;
 }
