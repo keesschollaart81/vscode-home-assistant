2c2735c232db0eaa24979456103e6c22acb57695
Franck Nijhof <git@frenck.dev>
2020-12-01 17:46:45 +0100

    Add deprecation warning for unit of measurement

--- src/language-service/src/schemas/integrations/core/uptime.ts
+++ src/language-service/src/schemas/integrations/core/uptime.ts
@@ -2,6 +2,7 @@
  * Uptime integration
  * Source: https://github.com/home-assistant/core/blob/dev/homeassistant/components/uptime/
  */
+import { Deprecated } from "../../types";
 import { PlatformSchema } from "../platform";
 
 export type Domain = "uptime";
@@ -18,4 +19,10 @@ export interface SensorPlatformSchema extends PlatformSchema {
    * https://www.home-assistant.io/integrations/uptime#name
    */
   name?: string;
+
+  /**
+   * DEPRECATED.
+   * This configuration option has been deprecated and can be removed from your configuration.
+   */
+  unit_of_measurement?: Deprecated;
 }
