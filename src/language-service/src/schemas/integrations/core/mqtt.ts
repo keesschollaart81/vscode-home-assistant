87a220ed94328c2ce39fb80dbfaad45941e02832
Franck Nijhof <git@frenck.dev>
2021-02-01 22:03:30 +0100

    Fix MQTT tilt_command_topic type (#957)

--- src/language-service/src/schemas/integrations/core/mqtt.ts
+++ src/language-service/src/schemas/integrations/core/mqtt.ts
@@ -1299,7 +1299,7 @@ export interface CoverPlatformSchema extends PlatformSchema {
    * The MQTT topic to publish commands to control the cover tilt.
    * https://www.home-assistant.io/integrations/cover.mqtt/#tilt_command_topic
    */
-  tilt_command_topic?: Integer;
+  tilt_command_topic?: string;
 
   /**
    * Flag that determines if open/close are flipped; higher values toward closed and lower values toward open.
