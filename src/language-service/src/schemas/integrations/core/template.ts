d5819744c6e154a36096f3dbacbe6bc4f29af728
Ryan Johnson <43426700+ryanjohnsontv@users.noreply.github.com>
2021-03-19 14:24:25 -0500

    Update template.ts
    Add support for new fan template configuration variables

--- src/language-service/src/schemas/integrations/core/template.ts
+++ src/language-service/src/schemas/integrations/core/template.ts
@@ -9,6 +9,7 @@ import {
   DeviceClassesCover,
   IncludeList,
   IncludeNamed,
+  PositiveInteger,
   Template,
   TimePeriod,
 } from "../../types";
@@ -484,6 +485,24 @@ interface FanItem {
    * https://www.home-assistant.io/integrations/fan.template/#oscillating_template
    */
   oscillating_template?: Template;
+  
+  /**
+   * Defines a template to get the speed percentage of the fan.
+   * https://www.home-assistant.io/integrations/fan.template/#percentage_template
+   */
+  percentage_template?: Template;
+  
+  /**
+   * Defines a template to get the preset mode of the fan.
+   * https://www.home-assistant.io/integrations/fan.template/#preset_mode_template
+   */
+  preset_mode_template?: Template;
+  
+  /**
+   * List of preset modes the fan is capable of. This is an arbitrary list of strings and must not contain any speeds.
+   * https://www.home-assistant.io/integrations/fan.template/#preset_modes
+   */
+  preset_modes?: string[];
 
   /**
    * Defines an action to run when the fan is given a direction command.
@@ -496,24 +515,33 @@ interface FanItem {
    * https://www.home-assistant.io/integrations/fan.template/#set_oscillating
    */
   set_oscillating?: Action | Action[];
+  
+  /**
+   * Defines an action to run when the fan is given a speed percentage command.
+   * https://www.home-assistant.io/integrations/fan.template/#set_percentage
+   */
+  set_percentage?: Action | Action[];
 
   /**
-   * Defines an action to run when the fan is given a speed command.
-   * https://www.home-assistant.io/integrations/fan.template/#set_speed
+   * DEPRECATED as of Home Assistant 2021.3.0
    */
-  set_speed?: Action | Action[];
+  set_speed?: Deprecated;
+  
+  /**
+   * The number of speeds the fan supports. Used to calculate the percentage step for the fan.increase_speed and fan.decrease_speed services.
+   * https://www.home-assistant.io/integrations/fan.template/#speed_count
+   */
+  speed_count?: PositiveInteger;
 
   /**
-   * Defines a template to get the speed of the fan.
-   * https://www.home-assistant.io/integrations/fan.template/#speed_template
+   * DEPRECATED as of Home Assistant 2021.3.0
    */
-  speed_template?: Template;
+  speed_template?: Deprecated;
 
   /**
-   * List of speeds the fan is capable of running at.
-   * https://www.home-assistant.io/integrations/fan.template/#speeds
+   * DEPRECATED as of Home Assistant 2021.3.0
    */
-  speeds: string[];
+  speeds: Deprecated;
 
   /**
    * Defines an action to run when the fan is turned off.
