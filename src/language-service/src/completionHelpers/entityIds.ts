9156f60fbd61ebb29a1cf1c0c58124b9efe82090
Nicholai Nissen <nicholainissen@gmail.com>
2021-05-10 12:26:11 +0200

    fix: Add comma-dangle

--- src/language-service/src/completionHelpers/entityIds.ts
+++ src/language-service/src/completionHelpers/entityIds.ts
@@ -22,7 +22,7 @@ export class EntityIdCompletionContribution implements JSONWorkerContribution {
     "zone",
     "zones",
     "light",
-    "lights"
+    "lights",
   ];
 
   constructor(private haConnection: IHaConnection) {}
