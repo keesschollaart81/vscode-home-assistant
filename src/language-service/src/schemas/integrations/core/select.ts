8acf8a989f72ec501bf62051f1e3c0c5b58ff68f
Franck Nijhof <git@frenck.dev>
2021-06-23 15:16:34 +0200

    Add support for MQTT select entities

---
+++ src/language-service/src/schemas/integrations/core/select.ts
@@ -0,0 +1,23 @@
+/**
+ * Select integration
+ * Source: https://github.com/home-assistant/core/blob/dev/homeassistant/components/select/__init__.py
+ */
+import { IncludeList } from "../../types";
+import { PlatformSchema } from "../platform";
+import { SelectPlatformSchema as MQTTSelectPlatformSchema } from "./mqtt";
+
+export type Domain = "number";
+export type Schema = Item[] | IncludeList;
+export type File = Item | Item[];
+
+/**
+ * @TJS-additionalProperties true
+ */
+interface OtherPlatform extends PlatformSchema {
+  /**
+   * @TJS-pattern ^(?!(mqtt)$)\w+$
+   */
+  platform: string;
+}
+
+type Item = MQTTSelectPlatformSchema | OtherPlatform;
