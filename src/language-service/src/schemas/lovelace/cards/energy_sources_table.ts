cb4cbc653c78ba23f60789161675e3dc5e6a4ae4
Franck Nijhof <git@frenck.dev>
2021-08-03 11:51:41 +0200

    Add Lovelace energy cards

---
+++ src/language-service/src/schemas/lovelace/cards/energy_sources_table.ts
@@ -0,0 +1,17 @@
+/**
+ * Lovelace Energy sources table Card
+ * Sources:
+ *  - https://github.com/home-assistant/frontend/blob/dev/src/panels/lovelace/cards/energy/hui-energy-sources-table-card.ts
+ */
+export interface Schema {
+  /**
+   * The energy sources table card shows all your energy sources, and the corresponding amount of energy.
+   * https://www.home-assistant.io/lovelace/energy/#energy-sources-table
+   */
+  type: "energy-sources-table";
+
+  /**
+   * The card title.
+   */
+  title?: string;
+}
