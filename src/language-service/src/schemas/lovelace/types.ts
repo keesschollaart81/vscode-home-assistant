ee885579bf9abf24881dcaa16fb2384c575833d4
Franck Nijhof <git@frenck.dev>
2021-08-03 13:12:31 +0200

    Add support for the Lovelace Statistics Graph card

--- src/language-service/src/schemas/lovelace/types.ts
+++ src/language-service/src/schemas/lovelace/types.ts
@@ -42,6 +42,7 @@ export type Card =
   | cards.PlantStatus.Schema
   | cards.Sensor.Schema
   | cards.ShoppingList.Schema
+  | cards.StatisticsGraph.Schema
   | cards.Thermostat.Schema
   | cards.VerticalStack.Schema
   | cards.WeatherForecast.Schema;
