1c25f732eefcc54d8d50fec47c32e2457dbad608
Franck Nijhof <git@frenck.dev>
2021-11-12 09:41:00 +0100

    Prettier

--- src/language-service/src/schemas/types.ts
+++ src/language-service/src/schemas/types.ts
@@ -112,10 +112,7 @@ export type DeviceClassesSensor =
 /**
  * From: https://github.com/home-assistant/core/blob/dev/homeassistant/components/sensor/__init__.py
  */
-export type StateClassesSensor = 
- | "measurement"
- | "total"
- | "total_increasing";
+export type StateClassesSensor = "measurement" | "total" | "total_increasing";
 
 /**
  * From: https://github.com/home-assistant/core/blob/dev/homeassistant/components/switch/__init__.py
