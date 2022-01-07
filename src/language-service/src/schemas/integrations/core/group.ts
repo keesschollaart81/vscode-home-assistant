5108252bbf4c337c6ebf06755565654a99505f82
Franck Nijhof <git@frenck.dev>
2022-01-07 21:06:31 +0100

    Add support for binary sensor groups

--- src/language-service/src/schemas/integrations/core/group.ts
+++ src/language-service/src/schemas/integrations/core/group.ts
@@ -2,7 +2,7 @@
  * Group integration
  * Source: https://github.com/home-assistant/core/blob/dev/homeassistant/components/group/__init__.py
  */
-import { Entities, IncludeNamed } from "../../types";
+import { DeviceClassesBinarySensor, Entities, IncludeNamed } from "../../types";
 import { PlatformSchema } from "../platform";
 
 export type Domain = "group";
@@ -34,6 +34,44 @@ interface Item {
    * https://www.home-assistant.io/integrations/group#name
    */
   name?: string;
+}
+
+export interface BinarySensorPlatformSchema extends PlatformSchema {
+  /**
+   * The group binary_sensor platform lets you combine multiple binary_sensors into one entity.
+   * https://www.home-assistant.io/integrations/binary_sensor.group
+   */
+  platform: "group";
+
+  /**
+   * Set this to true if the group state should only turn on if all grouped entities are on, false otherwise.
+   * https://www.home-assistant.io/integrations/binary_sensor.group/#entities
+   */
+  all?: boolean;
+
+  /**
+   * Set this to true if the group state should only turn on if all grouped entities are on, false otherwise.
+   * https://www.home-assistant.io/integrations/binary_sensor.group#device_class
+   */
+  device_class?: DeviceClassesBinarySensor;
+
+  /**
+   * A list of entities to be included in the binary sensor group.
+   * https://www.home-assistant.io/integrations/binary_sensor.group/#entities
+   */
+  entities: Entities;
+
+  /**
+   * The name of the binary sensor group. Defaults to "Binary Sensor Group”.
+   * https://www.home-assistant.io/integrations/binary_sensor.group#name
+   */
+  name?: string;
+
+  /**
+   * An ID that uniquely identifies this binary sensor group. If two binary sensors have the same unique ID, Home Assistant will raise an error.
+   * https://www.home-assistant.io/integrations/binary_sensor.group#unique_id
+   */
+  unique_id?: string;
 }
 
 export interface LightPlatformSchema extends PlatformSchema {
