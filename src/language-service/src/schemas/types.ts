55712ccb8d1ba387b1216d39c3ccb64c4cb1709e
Franck Nijhof <git@frenck.dev>
2022-04-26 10:48:23 +0200

    Prettier

--- src/language-service/src/schemas/types.ts
+++ src/language-service/src/schemas/types.ts
@@ -171,14 +171,13 @@ export type DeviceTrackerEntities = string | string[];
 /**
  * @TJS-pattern ^calendar\.(?!_)[\da-z_]+(?<!_)$
  */
- export type CalendarEntity = string;
+export type CalendarEntity = string;
 
- /**
-  * @TJS-pattern ^calendar\.(?!_)[\da-z_]+(?<!_)\s?(?:,\s?calendar\.(?!_)[\da-z_]+(?<!_))*$
-  * @items.pattern ^calendar\.(?!_)[\da-z_]+(?<!_)$
-  */
- export type CalendaraEntities = string | string[];
- 
+/**
+ * @TJS-pattern ^calendar\.(?!_)[\da-z_]+(?<!_)\s?(?:,\s?calendar\.(?!_)[\da-z_]+(?<!_))*$
+ * @items.pattern ^calendar\.(?!_)[\da-z_]+(?<!_)$
+ */
+export type CalendaraEntities = string | string[];
 
 /**
  * @TJS-pattern ^camera\.(?!_)[\da-z_]+(?<!_)$
